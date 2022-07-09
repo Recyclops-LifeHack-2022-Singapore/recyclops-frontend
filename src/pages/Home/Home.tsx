2dd71ba8f775e2ad992d1d14059e00610e84559d
ShenyiCui <shenyicui@outlook.com>
2022-07-09 22:08:29 +0800

    Change Name

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 
 import { IonContent, IonPage } from '@ionic/react';
 import AppToolbar from '@/components/AppToolbar';
-import BlockButton from '@/components/BlockButton';
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
+    <IonPage className='bg-[#F4F5F8]'>
       <AppToolbar />
-      <IonContent fullscreen className='relative'>
-        <BlockButton title='hello' />
-      </IonContent>
+      <IonContent fullscreen className='relative'></IonContent>
     </IonPage>
   );
 };

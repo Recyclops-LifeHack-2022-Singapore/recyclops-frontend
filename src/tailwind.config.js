7d95d9d8ae003c3e4376d431387c57d0a616220d
ShenyiCui <shenyicui@outlook.com>
2022-07-09 22:12:19 +0800

    fix lint errors

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -2,7 +2,6 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   important: true,
-  darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: { Inter: ['Inter', 'sans-serif'], epilogue: ['Epilogue', 'sans-serif'] },
     extend: {
